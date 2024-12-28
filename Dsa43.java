8ac2147859155f533ef3edf3c022c2687740dbd4
Anujpal05 <ap091453@gmail.com>
2024-12-28 17:57:10 +0530

    Binary Search

---
+++ Dsa43.java
@@ -0,0 +1,24 @@
+public class Dsa43 {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { -1, 0, 3, 5, 9, 12 };
+        int i = search(nums, 9);
+        System.out.println(i);
+    }
+}
