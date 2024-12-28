cfa473efe4576f42078c1a9ab4592cc1fe9fcb47
Anujpal05 <ap091453@gmail.com>
2024-12-28 20:25:04 +0530

    Word Break

---
+++ Dsa45.java
@@ -0,0 +1,72 @@
+// Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Example 1:
+// Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+// Output: ["cats and dog","cat sand dog"]
+
+// Example 2:
+// Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+// Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+// Explanation: Note that you are allowed to reuse a dictionary word.
+
+// Example 3:
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: []
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Dsa45 {
+
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Map<String, List<String>> memo = new HashMap<>();
+        return backtrack(s, wordSet, memo);
+    }
+
+    public static List<String> backtrack(String s, Set<String> wordSet, Map<String, List<String>> memo) {
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+        List<String> result = new ArrayList<>();
+
+        if (s.isEmpty()) {
+            result.add("");
+        }
+
+        for (int end = 1; end <= s.length(); end++) {
+            String prefix = s.substring(0, end);
+            if (wordSet.contains(prefix)) {
+                List<String> suffixSentences = backtrack(s.substring(end), wordSet, memo);
+                for (String sentence : suffixSentences) {
+                    if (sentence.isEmpty()) {
+                        result.add(prefix);
+                    } else {
+                        result.add(prefix + " " + sentence);
+                    }
+                }
+            }
+        }
+
+        memo.put(s, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("cat");
+        wordDict.add("cats");
+        wordDict.add("and");
+        wordDict.add("sand");
+        wordDict.add("dog");
+
+        String s = "catsanddog";
+        System.out.println(wordBreak(s, wordDict));
+        ;
+    }
+}
