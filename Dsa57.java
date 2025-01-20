6c51863300cb41ff1d0b14b1ef830cb8f5b358b2
Anujpal05 <ap091453@gmail.com>
2025-01-20 22:57:38 +0530

    Find Majority vote

--- Dsa57.java
+++ Dsa57.java
@@ -1,4 +1,5 @@
 import java.util.*;
+// You are given an array of integer arr[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
 
 public class Dsa57 {
 
